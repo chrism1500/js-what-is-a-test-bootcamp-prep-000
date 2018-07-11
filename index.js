a790b0ed5e89e81410eaac3009eff2b0f781c690
morales.chris1989@gmail.com <morales.chris1989@gmail.com>
2018-07-11 13:18:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 console.log(message)
 module.exports = {name, height, message}
-var message = `${name} is ${height} inches tall`
-console.log(message)  
-module.exports = {name, height, message}
